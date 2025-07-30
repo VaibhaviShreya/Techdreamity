d6af5acf2d5a2235f27a668be560f54c3efeee97
vaibhavi_shreya _ <127093216+VaibhaviShreya@users.noreply.github.com>
2025-07-30 13:29:32 +0530

    Update Contact.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/send-email", {
+    const response = await fetch("https://techdreamity.onrender.com/send-email", {
       method: "POST",
       body: formData,
     });
